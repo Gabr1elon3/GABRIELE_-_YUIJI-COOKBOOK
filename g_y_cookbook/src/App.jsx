a719bc8084d6ceba0ab0c44e5f186c51c31549d0
Gabriele Capezzuto <gabricap7@gmail.com>
2025-03-27 19:14:37 +0100

    new app.jsx

--- g_y_cookbook/src/App.jsx
+++ g_y_cookbook/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-<<<<<<< HEAD
       <Sidebar />
       <div className="content" style={{display: "flex", border: "1px solid black"}}>
         {/* Main content can go here */}
@@ -24,48 +23,6 @@ function App() {
 
       <h2>Recipes</h2>
       <List recipes = {recetas}/>
-=======
-      
-      <div className="content" style={{display: "flex", border:"1px solid black"}}>
-        {/* Main content can go here */}
-        <Sidebar />
-        <div><h2>Recipes</h2>
-      <ul>
-        {recipesArr.map((recipe) => {
-
-          return (
-            
-              (recipe.servings >= 1 )? 
-              (
-           
-                <li>
-                  <p>{recipe.name}</p>
-                  <p>Calories: {recipe.calories}</p>
-                  <img src= {recipe.image} width = "100px" height = "100px"/>
-                  <p>Servings: {recipe.servings}</p>
-                  <button>Delete recipe</button>
-                </li>
-
-
-              ) :
-
-               (
-                <h2>Not enough ingredients</h2>
-              )
-              
-            
-          )
-
-         
-         
-        })}
-      </ul></div>
-        
-      </div>
-      <Footer />
-
-      
->>>>>>> 62cf269a717c5b32e2feec8630dc836b1d2de983
 
     </div>
   );
